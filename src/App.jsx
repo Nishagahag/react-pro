2f0de539becbfb76c60153bda3c85ed9c6829076
nishagahag <nishagaha62@gmail.com>
2025-01-02 10:32:14 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card, { Abc } from "./components/card";
+function App() {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Abc/>
+      <Abc/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,22 @@
+const Card = () => {
+  return (
+    <>
+      <h1 className="text-xl text-[#000000] ">Pizza</h1>
+      <img src="piz.jpg" alt="pizza" className="h-4 " />
+      <p className="text-sm">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+        totam. Laborum a fugiat deleniti itaque?
+      </p>
+    </>
+  );
+};
+
+const Abc = () => {
+  return (
+    <>
+      <h1>Footer</h1>
+    </>
+  );
+};
+export default Card;
+export { Abc };
